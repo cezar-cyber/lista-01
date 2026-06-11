9463b98b135fe07751f409b2e5c3a87de89db8ab
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-11 20:49:22 -0300

    q1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Idade do Visitante: ");
+int Idade = int.Parse(Console.ReadLine()!);
+
+if (Idade >= 16) {
+    Console.WriteLine("Entrada liberada.");
+} else{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}
