a02d75b957f828f1e18e33765f2db84f0dbe9086
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-15 20:55:22 -0300

    q12

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿Console.WriteLine("Informe o número final da contagem: ");
-int final = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("Informe o número inicial da contagem regressiva: ");
+int inicial = int.Parse(Console.ReadLine()!);
 
-for (int i = 1; i <= final; i++) {
+for (int i = inicial; i >= 0; i--) {
     Console.WriteLine(i);
 }
+
+Console.WriteLine("Fim da contagem.");
