cfc3bfcbbaf663fb9b82dbcf811ae0345ea0165f
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-15 20:40:48 -0300

    q9

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
-﻿Console.WriteLine("Informe a primeira pontuação: ");
-int p1 = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Informe a segunda pontuação: ");
-int p2 = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Informe a terceira pontuação: ");
-int p3 = int.Parse(Console.ReadLine()!);
-
-if (p1 == p2 && p2 == p3) {
-    Console.WriteLine("As três pontuações são iguais.");
-} else if (p1 >= p2 && p1 >= p3) {
-    Console.WriteLine($"Maior pontuação: {p1}");
-} else if (p2 >= p1 && p2 >= p3) {
-    Console.WriteLine($"Maior pontuação: {p2}");
+if (numero % 2 == 0) {
+    Console.WriteLine("O número informado é par.");
 } else {
-    Console.WriteLine($"Maior pontuação: {p3}");
+    Console.WriteLine("O número informado é ímpar.");
 }
