fc6794c1ead4578f9a477431d6de6c9a67ef7149
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-15 20:59:09 -0300

    q13

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿Console.WriteLine("Informe o número inicial da contagem regressiva: ");
-int inicial = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("Informe o número da tabuada: ");
+int numero = int.Parse(Console.ReadLine()!);
 
-for (int i = inicial; i >= 0; i--) {
-    Console.WriteLine(i);
+for (int i = 1; i <= 10; i++) {
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
 }
-
-Console.WriteLine("Fim da contagem.");
