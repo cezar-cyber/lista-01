c9063b1b61c0816877bc2c90ce6f30814816e7b7
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-15 21:04:31 -0300

    q17

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿Console.WriteLine("Informe a senha: ");
-int senha = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("Informe uma nota de 0 a 10: ");
+int nota = int.Parse(Console.ReadLine()!);
 
-while (senha != 1234) {
-    Console.WriteLine("Senha incorreta. Tente novamente.");
-    Console.WriteLine("Informe a senha: ");
-    senha = int.Parse(Console.ReadLine()!);
+while (nota < 0 || nota > 10) {
+    Console.WriteLine("Nota inválida. Informe novamente.");
+    Console.WriteLine("Informe uma nota de 0 a 10: ");
+    nota = int.Parse(Console.ReadLine()!);
 }
 
-Console.WriteLine("Acesso permitido.");
+Console.WriteLine($"Nota registrada: {nota}");
