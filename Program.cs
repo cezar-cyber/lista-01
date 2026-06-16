aa63b7ca7c80db90f73869acfc37b0bc6cc8f29f
Cezar Souza <cesar_v_souza@estudante.sesisenai.org.br>
2026-06-15 21:02:11 -0300

    q15

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿Console.WriteLine("Informe o primeiro valor: ");
-int v1 = int.Parse(Console.ReadLine()!);
+﻿int positivos = 0;
+int negativos = 0;
+int zeros = 0;
 
-Console.WriteLine("Informe o segundo valor: ");
-int v2 = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < 10; i++) {
+    Console.WriteLine("Informe um número: ");
+    int numero = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Informe o terceiro valor: ");
-int v3 = int.Parse(Console.ReadLine()!);
+    if (numero > 0) {
+        positivos++;
+    } else if (numero < 0) {
+        negativos++;
+    } else {
+        zeros++;
+    }
+}
 
-Console.WriteLine("Informe o quarto valor: ");
-int v4 = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Informe o quinto valor: ");
-int v5 = int.Parse(Console.ReadLine()!);
-
-int soma = v1 + v2 + v3 + v4 + v5;
-double media = soma / 5.0;
-
-Console.WriteLine($"Soma dos valores: {soma}");
-Console.WriteLine($"Média dos valores: {media}");
+Console.WriteLine($"Quantidade de positivos: {positivos}");
+Console.WriteLine($"Quantidade de negativos: {negativos}");
+Console.WriteLine($"Quantidade de zeros: {zeros}");
